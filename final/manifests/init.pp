eab077d057c0a92f4f2d25c6966aa6bd8c76c986
VladCharches <uladzislau_charches@epam.com>
2016-09-26 11:37:16 +0300

    v1.1

--- final/manifests/init.pp
+++ final/manifests/init.pp
@@ -1,8 +1,11 @@
+# Check pupet server and run class for installation agent or server
+#
 class final {
-  if $::hostname == pserver {
+  if $::is_puppetmaster == 'true' {
     include final::pserver
   }
   else {
     include final::node1
   }
 }
+
--- final/manifests/node1.pp
+++ final/manifests/node1.pp
@@ -1,18 +1,21 @@
+# Class install node1
+#
 class final::node1 {
-		
-	package { 'puppet-repo': 
-		ensure  => installed,
-		provider => rpm,
-		source => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm'
-	}
-	
-	package { 'puppet-agent':
-		ensure => 'latest',
-		require => Package['puppet-repo'],
-	}
-	
-	service { 'puppet':
-		ensure => 'running',
-		require => Package['puppet-agent'],    
-	}
+
+  package { 'puppet-repo':
+    ensure   => installed,
+    provider => rpm,
+    source   => 'https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm'
+  }
+
+  package { 'puppet-agent':
+    ensure  => 'latest',
+    require => Package['puppet-repo'],
+  }
+
+  service { 'puppet':
+    ensure  => 'running',
+    require => Package['puppet-agent'],
+  }
 }
+
